ae0d4948facd521eb2a0321e00d4f67e01deb53e
Matt Hinegardner <matt.hinegardner@oracle.com>
2019-11-01 19:03:05 -0400

    Updates

---
+++ precache-manifest.40f35f44ef560f135a059ff949ca2689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d93f08c28c71df5008514592a397c7",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "2c5401ef01f4e2fcf927",
+    "url": "/my-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "947abe3e4a62357c714a",
+    "url": "/my-app/static/js/2.017bb613.chunk.js"
+  },
+  {
+    "revision": "2c5401ef01f4e2fcf927",
+    "url": "/my-app/static/js/main.6bdd2bce.chunk.js"
+  },
+  {
+    "revision": "4883f767f8b6e335f294",
+    "url": "/my-app/static/js/runtime-main.74992542.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-app/static/media/logo.25bf045c.svg"
+  }
+]);
